1a36ad7331ae3c147e2c4bce5e9a69074dcb6619
sajib9090 <nispap.devil.sajib@gmail.com>
2023-09-29 15:28:17 +0600

    user loading dashboard refresh prblm solved

--- front-end/src/Routes/AdminRoute.jsx
+++ front-end/src/Routes/AdminRoute.jsx
@@ -1,21 +1,67 @@
+// /* eslint-disable react/prop-types */
+// import { Navigate, useLocation } from "react-router-dom";
+// import { useContext } from "react";
+// import { AuthContext } from "../GlobalContext/AuthProvider";
+// import Loader from "../components/Loader/Loader";
+// import { useUserContext } from "../GlobalContext/UserContext";
+
+// const AdminRoute = ({ children }) => {
+//   const { user, loading, logOut } = useContext(AuthContext);
+//   const { singleUser } = useUserContext();
+//   const location = useLocation();
+
+//   if (loading) {
+//     return <Loader />;
+//   }
+//   if (user?.email && singleUser?.email && singleUser?.isAdmin === true) {
+//     return children;
+//   } else {
+//     logOut();
+//   }
+//   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+// };
+
+// export default AdminRoute;
+
 /* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../GlobalContext/AuthProvider";
 import Loader from "../components/Loader/Loader";
 import { useUserContext } from "../GlobalContext/UserContext";
 
 const AdminRoute = ({ children }) => {
-  const { user, loading } = useContext(AuthContext);
+  const { user, loading, logOut } = useContext(AuthContext);
   const { singleUser } = useUserContext();
   const location = useLocation();
+  const [isLoadingUser, setIsLoadingUser] = useState(true);
 
-  if (loading) {
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        await singleUser();
+        setIsLoadingUser(false);
+      } catch (error) {
+        setIsLoadingUser(false);
+        logOut();
+      }
+    };
+
+    if (loading) {
+      loadUser();
+    } else {
+      setIsLoadingUser(false);
+    }
+  }, [loading, singleUser, logOut]);
+
+  if (isLoadingUser) {
     return <Loader />;
   }
+
   if (user?.email && singleUser?.email && singleUser?.isAdmin === true) {
     return children;
   }
+
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
